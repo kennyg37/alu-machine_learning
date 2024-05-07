031103a64bd4b7c8adc110626e52a43d1016b24b
kennyg37 <k.ganza@alustudent.com>
2024-05-07 11:03:17 +0200

    adding 0-line.py

---
+++ math/plotting/.ipynb_checkpoints/0-line-checkpoint.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+y = np.arange(0, 11) ** 3
+
+plt.plot([0, 10], y, 'r-')
+plt.show()
+
---
+++ math/plotting/0-line.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+y = np.arange(0, 11) ** 3
+x = np.arange(0, 11)
+
+plt.plot(x, y, 'r-')
+plt.show()
+
