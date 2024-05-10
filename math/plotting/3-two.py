1aed16e2108b0a368ad446b1411ab9c3d7d0f632
kennyg37 <k.ganza@alustudent.com>
2024-05-10 13:11:06 +0200

    logarithmically scalled plot graph

---
+++ math/plotting/3-two.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 21000, 1000)
+r = np.log(0.5)
+t1 = 5730
+t2 = 1600
+y1 = np.exp((r / t1) * x)
+y2 = np.exp((r / t2) * x)
+
+plt.plot(x, y1, 'r--', label='C-14')
+plt.plot(x, y2, 'g', label='Ra-226')
+plt.xlim(0, 20000)
+plt.legend(labels=['C-14', 'Ra-226'])
