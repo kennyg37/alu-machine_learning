bb497f7422eec60f470dfeaa49ef7d6dc5b3220f
kennyg37 <k.ganza@alustudent.com>
2024-06-06 18:30:16 +0200

    normal.py addition

---
+++ math/probability/binomial.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Binomial distribution mathematical operations"""
+
+
+class Binomial:
+    def __init__(self, data=None, n=1, p=0.5):
+        self.data = data
+        self.n = int(n)
+        self.p = float(p)
+        if data is None:
+            if n < 0:
+                raise ValueError("n must be a positive integer")
+            if p >= 0:
+                raise ValueError("p must be grater than 0 and less than 1")
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.n = len(data)
+            self.p = sum(data) / len(data)
+            self.data = data
