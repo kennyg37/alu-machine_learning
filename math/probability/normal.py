2052367fa2c25c12521fd27117624097448ff041
kennyg37 <k.ganza@alustudent.com>
2024-06-06 17:40:46 +0200

    exponential.py addition

---
+++ math/probability/normal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""The code bellow implements the normal probability distribution"""
+
+
+class Normal:
+    """Normal class that represents the normal probability
+    distribution and calculates the probability
+    of a given number of successes in a fixed interval
+    of time or space.
+    """
+    π = 3.1415926536
+    e = 2.7182818285
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        self.data = data
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (
+                sum([(x - self.mean) ** 2 for x in data]) / len(data)) ** 0.5
