15072a0878faa14082241495e762e50c427290d0
kennyg37 <k.ganza@alustudent.com>
2024-06-06 12:42:34 +0200

    poisson probabiliy computation in python

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -17,7 +17,7 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = (1/(sum(data) / len(data)))
+            self.lambtha = sum(data) / len(data)
             
             
         
