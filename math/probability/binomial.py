ddccf32fdea443b73d81474cebac7973ca035057
kennyg37 <k.ganza@alustudent.com>
2024-06-07 11:02:42 +0200

    normal.py addition

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -31,7 +31,7 @@ class Binomial:
 
             self.n = int(n)
             self.p = float(p)
-    
+
     def pmf(self, k):
         """Calculates the value of the PMF for a given number of successes
 
