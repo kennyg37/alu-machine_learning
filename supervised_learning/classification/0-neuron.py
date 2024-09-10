4927856ccd4a37d5f13a78cf93aa04aea415b20a
kennyg37 <k.ganza@alustudent.com>
2024-09-10 11:21:04 +0200

    fixing pep8 errors

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -21,12 +21,12 @@ class Neuron:
     """
 
     def __init__(self, nx):
-        self.nx = nx
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
-
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
+
+        self.nx = nx
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
