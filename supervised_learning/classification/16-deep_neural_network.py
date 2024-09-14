fd0a1ebcbb42ead1ca8497d83a8db81526f1dcba
kennyg37 <k.ganza@alustudent.com>
2024-09-14 11:25:22 +0200

    fixing typo in typeerror

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
-
-"""
-simplest neural network with no hidden layer
-
+""" Deep Neural Network
 """
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
+    """ Class that defines a deep neural network performing binary
+        classification.
+    """
+
     def __init__(self, nx, layers):
-        """
-        nx is the number of input features
-        layers is a list representing the number of nodes in each layer of the network
-        The weights of the network should be initialized using the He et al. method
-        The biases of the network should be initialized to 0
+        """ Instantiation function
+
+        Args:
+            nx (int): number of input features
+            layers (list): representing the number of nodes in each layer of
+                           the network
         """
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
 
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+        if len(layers) < 1:
             raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
@@ -31,10 +35,15 @@ class DeepNeuralNetwork:
         for i in range(self.L):
             if not isinstance(layers[i], int) or layers[i] < 1:
                 raise TypeError('layers must be a list of positive integers')
+
             if i == 0:
-                self.__weights['W' + str(i + 1)] = np.random.randn(
+                # He et al. initialization
+                self.weights['W' + str(i + 1)] = np.random.randn(
                     layers[i], nx) * np.sqrt(2 / nx)
             else:
-                self.__weights['W' + str(i + 1)] = np.random.randn(
+                # He et al. initialization
+                self.weights['W' + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+            # Zero initialization
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
